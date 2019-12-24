29295e5c7e96da9ee5f819228abcb0bf3379b0ba
min1378 <qwes123@naver.com>
2019-12-24 16:59:20 +0900

    Jquery basic

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('main.js loaded')
+$(document).ready(function() {
+  'use strict';
+  // 'use strict' 는 자바스크립트 인터프리터에서 코드를 좀 더 엄격하게 처리하라는 뜻이다.
+  // 앞으로 만드는 자바스크립트 코드는 이 사이에 들어간다.
+  // HTML을 전부 불러왔는지 확인하는 console.log() => python의 print()
+  console.log('main.js loaded')
+})
