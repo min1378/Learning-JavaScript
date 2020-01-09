450729ba45f1bdcd57deb4b0ebf6286c822087dd
min1378 <qwes123@naver.com>
2020-01-09 19:56:05 +0900

    Data type var, const

--- basic.js
+++ basic.js
@@ -15,12 +15,31 @@
 
 // home();
 
-var list = document.querySelectorAll("li");
-//for (var i = 0; i < list.length; i++) { // i 값이 변경(var라서)되면서 4값을 공유하기 때문에 문제가 발생한다.
-for (let i = 0; i < list.length; i++) { // let으로 수정.
-    list[i].addEventListener("click", function() {
-        console.log(i + "번째 리스트 입니다.") // 무엇을 누르던지 4번째 리스트입니다. 가 출력된다. 그 이유는 closure 때문
-            // function이 갖고 있는 i는 콜백 바깥에 있는 변수를 참조하면서 기억하고 있다. i값은 클로져 변수라 할 수 있다.
+// var list = document.querySelectorAll("li");
+// //for (var i = 0; i < list.length; i++) { // i 값이 변경(var라서)되면서 4값을 공유하기 때문에 문제가 발생한다.
+// for (let i = 0; i < list.length; i++) { // let으로 수정.
+//     list[i].addEventListener("click", function() {
+//         console.log(i + "번째 리스트 입니다.") // 무엇을 누르던지 4번째 리스트입니다. 가 출력된다. 그 이유는 closure 때문
+//             // function이 갖고 있는 i는 콜백 바깥에 있는 변수를 참조하면서 기억하고 있다. i값은 클로져 변수라 할 수 있다.
 
-    });
+//     });
+// }
+
+function home() {
+    var homename = 'my house';
+    console.log(homename) // my house
+    homename = "your house";
+    console.log(homename) // your house
+    const homename2 = 'my house';
+    console.log(homename2) // my house
+        // homename2 = "your house"; // TypeError: Assignment to constant variable. const형엔 대입 불가.
+
+
+    const homename3 = [1, 2, 3, 3];
+    homename3 = ["1", "2"] // TypeError: Assignment to constant variable.
+    console.log(homename3)
+        // const를 기본으로 사용한다.
+        // 그런데 변경이 될 수 있는 변수는 let을 사용한다.
+        // var는 사용하지 않는다.
 }
+home();
